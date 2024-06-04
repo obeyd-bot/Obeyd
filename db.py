c5230765b6b34a9ba9ee9cff141ecac740fc9ca9
Ehsan Poursaeed <hsnprsd@gmail.com>
2024-06-05 02:47:09 +0330

    use aiopg.sa

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 import asyncio
+import os
 
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from aiopg.sa import create_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from models import Base
-import os
 
 db_host = os.environ["DB_HOST"]
 db_port = os.environ["DB_PORT"]
@@ -11,8 +12,8 @@ db_user = os.environ["DB_USER"]
 db_pass = os.environ["DB_PASS"]
 db_name = os.environ["DB_NAME"]
 
-engine = create_async_engine(
-    f"postgresql+aiopg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}",
+engine = create_engine(
+    f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}",
 )
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
