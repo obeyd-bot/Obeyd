d970945c5a5b25af10890da27e75ce97281d8295
Ehsan Poursaeed <hsnprsd@gmail.com>
2024-06-04 18:15:56 +0330

    joke handler

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 import logging
-import sys
 import os
+import random
+import sys
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
 # Bot token can be obtained via https://t.me/BotFather
@@ -16,40 +17,25 @@ TOKEN = os.environ["API_TOKEN"]
 
 dp = Dispatcher()
 
+outputs = [
+    "One",
+    "Two",
+    "Three",
+]
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer(f"سلام {html.bold(message.from_user.full_name)}!")
 
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+@dp.message(Command("joke"))
+async def joke_handler(message: Message) -> None:
+    await message.answer(random.choice(outputs))
 
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
