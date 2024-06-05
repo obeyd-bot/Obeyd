9abbbba38cd9f863473821fb23306e30ac779522
Ehsan Poursaeed <hsnprsd@gmail.com>
2024-06-06 03:00:47 +0330

    remove dead code

--- db.py
+++ db.py
@@ -1,18 +1,6 @@
-import asyncio
 import os
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-from models import Base
-
 engine = create_async_engine(os.environ["SQLALCHEMY_DATABASE_URI"])
 async_session = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def sync_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-if __name__ == "__main__":
-    asyncio.run(sync_db())
