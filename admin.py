9b214baccddf2ff1909de12d3b0a90205ee29f33
Ehsan Poursaeed <hsnprsd@gmail.com>
2024-06-06 16:54:29 +0330

    add seen jokes to admin

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 
-from models import Base, Joke, Like
+from models import Base, Joke, Like, SeenJoke
 
 if __name__ == "__main__":
     app = Flask(__name__)
@@ -20,5 +20,6 @@ if __name__ == "__main__":
     admin = Admin(app, name="obeyd", template_mode="bootstrap3")
     admin.add_view(ModelView(Joke, db.session))
     admin.add_view(ModelView(Like, db.session))
+    admin.add_view(ModelView(SeenJoke, db.session))
 
     app.run(host="0.0.0.0")
