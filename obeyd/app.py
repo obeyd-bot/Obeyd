a842073a7e2e0d6879eac442276bf58f552d0b65
hsnprsd <hsnprsd@gmail.com>
2024-06-15 01:09:48 +0330

    remove error handler

--- obeyd/app.py
+++ obeyd/app.py
@@ -729,34 +729,6 @@ async def recurring_joke_callback(context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
-    # traceback.format_exception returns the usual python message about an exception, but as a
-    # list of strings rather than a single string, so we have to join them together.
-    assert context.error
-
-    tb_list = traceback.format_exception(
-        None, context.error, context.error.__traceback__
-    )
-    tb_string = "".join(tb_list)
-
-    # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096 character limit.
-    update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    message = (
-        "An exception was raised while handling an update\n"
-        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-        "</pre>\n\n"
-        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-        f"<pre>{html.escape(tb_string)}</pre>"
-    )
-
-    # Finally, send the message
-    await context.bot.send_message(
-        chat_id=ALERTS_CHAT_ID, text=message, parse_mode=ParseMode.HTML
-    )
-
-
 if __name__ == "__main__":
     sentry_sdk.init(
         dsn="https://843cb5c0e82dfa5f061f643a1422a9cf@sentry.hamravesh.com/6750",
@@ -839,8 +811,6 @@ if __name__ == "__main__":
         CallbackQueryHandler(reviewjoke_callback_query_handler, pattern="^reviewjoke")
     )
     app.add_handler(InlineQueryHandler(inline_query_handler))
-
-    app.add_error_handler(error_handler)
 
     # jobs
     job_queue.run_repeating(
